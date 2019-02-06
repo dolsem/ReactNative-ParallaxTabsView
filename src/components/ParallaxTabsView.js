774d5ff1dbd01e967e1e3a133c3599bc2dea72ea
Denis Semenenko <dols3m@gmail.com>
2019-02-06 11:40:43 -0500

    commit

--- src/components/ParallaxTabsView.js
+++ src/components/ParallaxTabsView.js
@@ -49,6 +49,9 @@ const AnimatedImage = Animated.createAnimatedComponent(ImageBackground);
   scrollThreshold: PropTypes.number,
   /** Custom interval duration for calling onScrollPastThreshold() */
   scrollPastThresholdEventInterval: PropTypes.number,
+
+  misc1: PropTypes.component,
+  misc2: PropTypes.component.isRequired,
 })
 @defaultProps({
   tabHeadings: [],
